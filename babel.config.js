20b69a7456694c4378dde70d5cf4409e9bce86d0
Denner <denner.vidal@gmail.com>
2021-10-07 16:19:36 -0400

    build: babel is now the default transpiler

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,36 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  if (process.env.BABEL_ENV !== "build") {
+    return {
+      presets: ["babel-preset-expo"],
+    };
+  }
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        "@babel/env",
+        {
+          useBuiltIns: "usage",
+          corejs: 3,
+          targets: {
+            browsers: "Last 2 Chrome versions, Firefox ESR",
+            node: "current",
+          },
+        },
+      ],
+      [
+        "@babel/preset-react",
+        {
+          development: process.env.BABEL_ENV !== "build",
+        },
+      ],
+      "@babel/preset-typescript",
+    ],
+    env: {
+      build: {
+        ignore: [".expo", ".expo-shared", "assets", "App.tsx"],
+      },
+    },
+    ignore: ["node_modules"],
   };
 };
